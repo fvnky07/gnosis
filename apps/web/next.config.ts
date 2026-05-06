3f33d330f1c3a8d5527980aa9458187cf1cf816a
fvnky <brucewayne0002@proton.me>
2026-05-06 18:31:08 +0100

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@gnosis/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
